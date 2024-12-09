473504d61f43c46d2193020ccec0d2cc4988d454
Albert Osei Frimpong <alotab@yahoo.com>
2024-12-09 08:46:24 -0800

    update postdetail.jsx file

--- src/components/PostDetail.jsx
+++ src/components/PostDetail.jsx
@@ -17,6 +17,51 @@ const PostDetail = () => {
     const socialButtonDivRef = useRef(null);
     const buttonRef = useRef(null);
     const sociaMediaRefs = useRef([]);
+    const [isActive, setIsActive] = useState(false);
+    const [mouseOverTxt, setMouseOverTxt] = useState(false);
+    const [mouseOverComment, setMouseOverComment] = useState(false);
+    const [mouseOverBookmark, setMouseOverBookmark] = useState(false);
+    const [mouseOverLike, setMouseOverLike] = useState(false);
+
+
+
+    // Show text when mouse over share icon
+    const handleMouseOver = () => {
+        setMouseOverTxt(true);
+        setMouseOverBookmark(false);
+    };
+
+    // Show text when mouse over bookmark icon
+    const handleBookmarkOver = () => {
+        setMouseOverBookmark(true);
+        setMouseOverTxt(false);
+    }
+
+    const handleComment = () => {
+        setMouseOverComment(true);
+        setMouseOverBookmark(false);
+        setMouseOverTxt(false);
+        setMouseOverLike(false);
+    }
+
+    const handleLike = () => {
+        setMouseOverLike(true);
+        setMouseOverComment(false);
+        setMouseOverBookmark(false);
+        setMouseOverTxt(false);
+    }
+
+    // Hide text when mouse leaves the div
+    const handleMouseOut = () => {
+        setMouseOverTxt(false);
+        setMouseOverBookmark(false);
+        setMouseOverComment(false);
+        setMouseOverLike(false)
+    };
+
+
+
+
 
 
     // Navigate Blog Link to social Media apps function
@@ -53,12 +98,30 @@ const PostDetail = () => {
 
                 // Open a new window to share the URL
                 window.open(shareUrl, '_blank');
-            })
+            });
             
         });
     };
 
- 
+    // Function to copy Blog URL 
+    const permalinkShare = (e) => {
+        e.preventDefault();
+    
+        // Copy the URL to the clipboard
+        const url = window.location.href;
+        navigator.clipboard.writeText(url).then(() => {
+          // Show the message
+          setIsActive(true);
+    
+          // Hide the message after 1.5 seconds (1500 ms)
+          setTimeout(() => {
+            setIsActive(false);
+          }, 1500);
+        });
+    }
+
+
+    // Toggle over shareLink by clicking the Share icon 
     const onhandlePopOutButtons = () => {
         setSocialLinks(prevState => !prevState);
     };
@@ -101,104 +164,119 @@ const PostDetail = () => {
             { post 
                 ?   ( 
                     <div className="main-detail-post">
-                    <div className="detail-column">
-                        <div className="detail-post">
-                            <img src={post.image} alt={post.title} className="post-image" />
-                            <div className="main-post">
-                                <div className="main-post-author">
-                                    <img src={post.author.profile_picture} alt={post.author.first_name} />
-                                    <div className="author-name">
-                                        <p className="author-name-link">{post.author.first_name} {post.author.last_name}</p>
-                                        <p id="time-tag" className="publish">Posted on { post.publish} &middot; {post.read_time} read</p>
+                        <div className="detail-column">
+                            <div className="detail-post">
+                                <img src={post.image} alt={post.title} className="post-image" />
+                                <div className="main-post">
+                                    <div className="main-post-author">
+                                        <img src={post.author.profile_picture} alt={post.author.first_name} />
+                                        <div className="author-name">
+                                            <p className="author-name-link">{post.author.first_name} {post.author.last_name}</p>
+                                            <p id="time-tag" className="publish">Posted on { post.publish} &middot; {post.read_time} read</p>
+                                        </div>
+                                        {/* {% if request.user.is_authenticated %} */}
+                                        <div className="post-edit">
+                                            {/* <a href="{% url 'blog:post-update' post.slug post.pk %}">Update</a> */}
+                                        </div>
+                                        <div className="post-delete">
+                                            {/* <a href="{% url 'blog:post-delete' post.slug post.pk %}">Delete</a> */}
+                                        </div>
+                                        {/* {% endif %} */}
                                     </div>
-                                    {/* {% if request.user.is_authenticated %} */}
-                                    <div className="post-edit">
-                                        {/* <a href="{% url 'blog:post-update' post.slug post.pk %}">Update</a> */}
+                                    <div className="main-post-headline">
+                                        <h2>{post.title}</h2>
                                     </div>
-                                    <div className="post-delete">
-                                        {/* <a href="{% url 'blog:post-delete' post.slug post.pk %}">Delete</a> */}
+                                    <div className="main-post-tags">
+                                        <button><span id="tag-color" >react</span></button>
+                                        {/* {post.map((tag, idx) => {
+                                            <button><span id="tag-color">{ tag.tags }</span></button>
+                                        })} */}
+                                    
                                     </div>
-                                    {/* {% endif %} */}
-                                </div>
-                                <div className="main-post-headline">
-                                    <h2>{post.title}</h2>
-                                </div>
-                                <div className="main-post-tags">
-                                <button><span id="tag-color" >react</span></button>
-                                    {/* {post.map((tag, idx) => {
-                                        <button><span id="tag-color">{ tag.tags }</span></button>
-                                    })} */}
-                                
-                                </div>
-                                <div className="post-main-detail ck-content">
-                                    <p>{post.content}</p>
+                                    <div className="post-main-detail ck-content">
+                                        <p>{post.content}</p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
                 
                 
-                    {/* <!--===================== external- share ===============================--> */}
-                    <div className="external-share-container">
-                        <div className="share-scroll share-scroll-position">
-                            <div className="social-share">
-                                <i className="ri-heart-line share-like pop"></i>
-                                <div className="like-count"></div>
-                                {/* <i><FaRegHeart /></i> */}
-                                <p className="pop-like kat">Like this article</p>
-                            </div>
-                            <div className="social-share">
-                                <i className="ri-message-3-line share-comment pop"></i>
-                                <div className="comment-count"></div>
-                                {/* <i><BiCommentDots /></i> */}
-                                
-                                <p className="pop-comment kat">Write a comment</p>
-                            </div>
-                            <div className="social-share">
-                                <i className="ri-bookmark-line share-bookmark pop"></i>
-                                {/* <i><FaRegBookmark /></i> */}
-                                <p className="pop-bookmark kat">Add Bookmark</p>
-                            </div>
-                            <div className="social-share">
-                                {/* <i><GrShareOption /></i> */}
-                                <i className="ri-share-line share-share pop" ref={buttonRef} onClick={onhandlePopOutButtons} ></i>
-                            </div>
-
-                            {socialLinks && (
-                                <div ref={socialButtonDivRef} className="social-scroll-wrapper">
-                                    <div className="social-cs9 share-buttons">
-                                        <a href="#" className="share-button reddit" ref={(el) => sociaMediaRefs.current[0] = el} onClick={() => handleMediaLinks(0)}>
-                                            <i className="ri-reddit-fill"></i><p>Reddit</p>
-                                        </a>
-                                        <a href="#" className="share-button linkedin" ref={(el) => sociaMediaRefs.current[1] = el} onClick={() => handleMediaLinks(1)}>
-                                            <i className="ri-linkedin-box-fill"></i><p>Linkedin</p>
-                                        </a>
-                                        <a href="#" className="share-button twitch" ref={(el) => sociaMediaRefs.current[2] = el} onClick={() => handleMediaLinks(2)}>
-                                            <i className="ri-twitch-fill"></i><p>Twitch</p>
-                                        </a>
-                                        <a href="#" className="share-button whatsapp" ref={(el) => sociaMediaRefs.current[3] = el} onClick={() => handleMediaLinks(3)}>
-                                            <i className="ri-whatsapp-fill"></i><p>Whatsapp</p>
-                                        </a>
-                                        <a href="#" className="share-button facebook" ref={(el) => sociaMediaRefs.current[4] = el} onClick={() => handleMediaLinks(4)}>
-                                            <i className="ri-facebook-circle-fill"></i> <p>Facebook</p>
-                                        </a>
-                                        <a className="share-button twitter" href="#" ref={(el) => sociaMediaRefs.current[5] = el} onClick={() => handleMediaLinks(5)}>
-                                            <i className="ri-twitter-fill"></i><p>Twitter</p>
-                                        </a>
-                                        <a href="#">
-                                            <i className="ri-links-line"></i><p>Permalink</p>
-                                        </a>
-                                    </div>
+                        {/* <!--===================== external- share ===============================--> */}
+                        <div className="external-share-container">
+                            <div className="share-scroll share-scroll-position">
+                                <div className="social-share">
+                                    <i className="ri-heart-line share-like pop"
+                                        onMouseOver={handleLike}
+                                        onMouseOut={handleMouseOut}
+                                    
+                                    ></i>
+                                    <div className="like-count"></div>
+                                    <p className={mouseOverLike && !socialLinks ? 'pop-comment kat alert-active' : 'pop-like kat'}>Like this article</p>
                                 </div>
-                            )}
-                            
+                                <div className="social-share">
+                                    <i className="ri-message-3-line share-comment pop"
+                                        onMouseOver={handleComment}
+                                        onMouseOut={handleMouseOut}
+                                    
+                                    ></i>
+                                    <div className="comment-count"></div>
+                                    {/* <i><BiCommentDots /></i> */}
+                                    <p className={mouseOverComment && !socialLinks ? 'pop-comment kat alert-active' : 'pop-comment kat'}>Write a comment</p>
+                                </div>
+                                <div className="social-share">
+                                    <i className="ri-bookmark-line share-bookmark pop"
+                                        onMouseOver={handleBookmarkOver} 
+                                        onMouseOut={handleMouseOut}
+                                    ></i>
+                                    
+                                    <p className={mouseOverBookmark && !socialLinks ? 'pop-bookmark kat alert-active' : 'pop-bookmark kat'}>Add Bookmark</p>
+                                </div>
+                                <div className="social-share">
+                                    {/* <i><GrShareOption /></i> */}
+                                    <i className="ri-share-line share-share pop" 
+                                        ref={buttonRef} 
+                                        onClick={onhandlePopOutButtons} 
+                                        onMouseOver={handleMouseOver} 
+                                        onMouseOut={handleMouseOut}>
+                                    </i>
+                                    <p className={mouseOverTxt && !socialLinks ? 'pop-share kat alert-active' : 'pop-share kat'}>Share this article</p>
+                                </div>
 
-                            <div className="permalink-message">
-                                <i className="ri-checkbox-circle-line"></i>
-                                <p>Copied article link</p>
+                                {socialLinks && (
+                                    <div ref={socialButtonDivRef} className="social-scroll-wrapper">
+                                        <div className="social-cs9 share-buttons">
+                                            <a href="#" className="share-button reddit" ref={(el) => sociaMediaRefs.current[0] = el} onClick={() => handleMediaLinks(0)}>
+                                                <i className="ri-reddit-fill"></i><p>Reddit</p>
+                                            </a>
+                                            <a href="#" className="share-button linkedin" ref={(el) => sociaMediaRefs.current[1] = el} onClick={() => handleMediaLinks(1)}>
+                                                <i className="ri-linkedin-box-fill"></i><p>Linkedin</p>
+                                            </a>
+                                            <a href="#" className="share-button twitch" ref={(el) => sociaMediaRefs.current[2] = el} onClick={() => handleMediaLinks(2)}>
+                                                <i className="ri-twitch-fill"></i><p>Twitch</p>
+                                            </a>
+                                            <a href="#" className="share-button whatsapp" ref={(el) => sociaMediaRefs.current[3] = el} onClick={() => handleMediaLinks(3)}>
+                                                <i className="ri-whatsapp-fill"></i><p>Whatsapp</p>
+                                            </a>
+                                            <a href="#" className="share-button facebook" ref={(el) => sociaMediaRefs.current[4] = el} onClick={() => handleMediaLinks(4)}>
+                                                <i className="ri-facebook-circle-fill"></i> <p>Facebook</p>
+                                            </a>
+                                            <a className="share-button twitter" href="#" ref={(el) => sociaMediaRefs.current[5] = el} onClick={() => handleMediaLinks(5)}>
+                                                <i className="ri-twitter-fill"></i><p>Twitter</p>
+                                            </a>
+                                            <a href="#" className="permalink"  onClick={permalinkShare}>
+                                                <i className="ri-links-line"></i><p>Permalink</p>
+                                            </a>
+                                        </div>
+                                    </div>
+                                )}; 
+                                
+                          
+                                <div className={isActive ? 'permalink-message show-permalink-message' : 'permalink-message'}>
+                                    <i className="ri-checkbox-circle-line"></i>
+                                    <p>Copied article link</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
                     </div>
                     )
                 : 
